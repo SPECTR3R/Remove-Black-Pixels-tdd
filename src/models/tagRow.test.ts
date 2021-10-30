3eb64060c9ef6eeb2f76d14d16697b3586f02fe9
Armando del Río <adrio.coder@gmail.com>
2021-10-29 21:46:50 -0500

    refactor test

--- src/models/tagRow.test.ts
+++ src/models/tagRow.test.ts
@@ -2,14 +2,14 @@ import tagRow from './tagRow'
 
 describe('It tags adjacent non-zero values in an array', () => {
   it('It should not affect rows with no adjacent ', () => {
-    console.log(tagRow([1]))
-    const arr = [1, 0, 1, 0, 0]
-
-    expect(tagRow(arr)).toEqual([1, 0, 1, 0, 0])
+    expect(tagRow([1, 0, 1, 0, 0])).toEqual([1, 0, 1, 0, 0])
+    expect(tagRow([0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1])).toEqual([0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1])
+    expect(tagRow([1, 0, 0, 0, 0, 0, 1])).toEqual([1, 0, 0, 0, 0, 0, 1])
   })
 
-  it('Adjacent non-zero values should be taged as -1', () => {
-    const arr = [1, 0, 1, 1, 0]
-    expect(tagRow(arr)).toEqual([1, 0, -1, -1, 0])
+  it('Adjacent non-zero values should be tagged as -1', () => {
+    expect(tagRow([1, 0, 1, 1, 0])).toEqual([1, 0, -1, -1, 0])
+    expect(tagRow([1, 0, 1, 0, 1, 1, 1, 1, 0])).toEqual([1, 0, 1, 0, -1, -1, -1, -1, 0])
+    expect(tagRow([1, 0, 1, 1, 1, 0, 1, 1, 0])).toEqual([1, 0, -1, -1, -1, 0, -1, -1, 0])
   })
 })
--- src/models/tagRow.ts
+++ src/models/tagRow.ts
@@ -1,6 +1,11 @@
 export default function tagRow(arr: Array<number>): Array<number> {
-  for (let index = 0; index < arr.length; index++) {
-    const element = arr[index]
+  for (let index = 1; index < arr.length - 1; index++) {
+    const num1 = Math.abs(arr[index])
+    const num2 = Math.abs(arr[index + 1])
+    if (num1 === num2 && num1 !== 0) {
+      arr[index] = -num1
+      arr[index + 1] = -num2
+    }
   }
   return arr
 }
