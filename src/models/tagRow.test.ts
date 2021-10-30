03d9a337d2102b316fdcef40b00c776144c0aa39
Armando del Río <adrio.coder@gmail.com>
2021-10-29 23:11:47 -0500

    finish tagRow functions

--- src/models/tagRow.test.ts
+++ src/models/tagRow.test.ts
@@ -1,15 +1,27 @@
 import tagRow from './tagRow'
 
-describe('It tags adjacent non-zero values in an array', () => {
-  it('It should not affect rows with no adjacent ', () => {
+describe('It tags adjacent "ones" values in an array', () => {
+  it('It should not affect rows with no adjacen ones', () => {
     expect(tagRow([1, 0, 1, 0, 0])).toEqual([1, 0, 1, 0, 0])
     expect(tagRow([0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1])).toEqual([0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1])
     expect(tagRow([1, 0, 0, 0, 0, 0, 1])).toEqual([1, 0, 0, 0, 0, 0, 1])
   })
 
-  it('Adjacent non-zero values should be tagged as -1', () => {
+  it('Adjacent "ones" should be tagged as -1', () => {
     expect(tagRow([1, 0, 1, 1, 0])).toEqual([1, 0, -1, -1, 0])
     expect(tagRow([1, 0, 1, 0, 1, 1, 1, 1, 0])).toEqual([1, 0, 1, 0, -1, -1, -1, -1, 0])
     expect(tagRow([1, 0, 1, 1, 1, 0, 1, 1, 0])).toEqual([1, 0, -1, -1, -1, 0, -1, -1, 0])
+  })
+  it('Adjacent "ones" connected to a "one" in the left border should be tagged as 2', () => {
+    expect(tagRow([1, 1, 1, 1, 0])).toEqual([1, 2, 2, 2, 0])
+    expect(tagRow([1, 1, 1, 0, 1, 1, 1, 1, 0])).toEqual([1, 2, 2, 0, -1, -1, -1, -1, 0])
+    expect(tagRow([1, 1, 1, 1, 1, 0, 1, 1, 0])).toEqual([1, 2, 2, 2, 2, 0, -1, -1, 0])
+  })
+  it('Adjacent "ones" connected to a "one" in the rigth border should be tagged as 2', () => {
+    expect(tagRow([0, 0, 1, 1, 1])).toEqual([0, 0, 2, 2, 1])
+    expect(tagRow([0, 1, 1, 0, 1, 1, 1, 1, 1])).toEqual([0, -1, -1, 0, 2, 2, 2, 2, 1])
+    expect(tagRow([1, 1, 1, 1, 1, 0, 1, 1, 1])).toEqual([1, 2, 2, 2, 2, 0, 2, 2, 1])
+    expect(tagRow([0, 1, 1, 1, 1, 1, 1, 1, 1])).toEqual([0, 2, 2, 2, 2, 2, 2, 2, 1])
+
   })
 })
--- src/models/tagRow.ts
+++ src/models/tagRow.ts
@@ -1,11 +1,47 @@
 export default function tagRow(arr: Array<number>): Array<number> {
-  for (let index = 1; index < arr.length - 1; index++) {
+  const canBeLeftConnected = !!arr[0]
+  const canBeRigthConnected = !!arr[arr.length - 1]
+  const newArr = [...arr]
+
+  for (let index = 1; index < arr.length - 2; index++) {
     const num1 = Math.abs(arr[index])
     const num2 = Math.abs(arr[index + 1])
-    if (num1 === num2 && num1 !== 0) {
-      arr[index] = -num1
-      arr[index + 1] = -num2
+    if (num1 === num2 && num1 !== 0 && num1 !== 2) {
+      newArr[index] = -num1
+      newArr[index + 1] = -num2
     }
   }
-  return arr
+
+  if (canBeLeftConnected) {
+    for (let index = 1; index < newArr.length - 1; index++) {
+      const num1 = Math.abs(arr[index])
+      const num2 = Math.abs(arr[index + 1])
+
+      if (num1 === num2 && num1 !== 0) {
+        newArr[index] = 2
+        newArr[index + 1] = 2
+      } else {
+        break
+      }
+    }
+  }
+
+  for (let index = newArr.length - 2; index > 1; index--) {
+    if (canBeRigthConnected) {
+
+      const num1 = Math.abs(arr[index])
+      const num2 = Math.abs(arr[index - 1])
+
+      if (num1 === num2 && num1 !== 0) {
+        console.log('si don')
+
+        newArr[index] = 2
+        newArr[index - 1] = 2
+      } else {
+        break
+      }
+    }
+  }
+
+  return newArr
 }
