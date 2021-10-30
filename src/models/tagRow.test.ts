7823150360cfa83c3ef838c399a57d519affb7df
Armando del Río <adrio.coder@gmail.com>
2021-10-29 21:30:10 -0500

    add ts tdd project and create first two test

--- src/models/tagRow.test.ts
+++ src/models/tagRow.test.ts
@@ -1,10 +1,15 @@
 import tagRow from './tagRow'
 
-describe('It should not affect rows with no adjacent 1', () => {
-  it('tagRow', () => {
+describe('It tags adjacent non-zero values in an array', () => {
+  it('It should not affect rows with no adjacent ', () => {
     console.log(tagRow([1]))
     const arr = [1, 0, 1, 0, 0]
 
     expect(tagRow(arr)).toEqual([1, 0, 1, 0, 0])
+  })
+
+  it('Adjacent non-zero values should be taged as -1', () => {
+    const arr = [1, 0, 1, 1, 0]
+    expect(tagRow(arr)).toEqual([1, 0, -1, -1, 0])
   })
 })
