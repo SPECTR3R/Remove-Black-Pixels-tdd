4ab81ee5306cea0ef3782ed8d066eef5d192dcca
Armando del Río <adrio.coder@gmail.com>
2021-10-30 15:02:26 -0500

    Making progress, :D

---
+++ src/models/RemoveBlackPixels.test.ts
@@ -0,0 +1,46 @@
+import { RemoveBlackPixels } from './RemoveBlackPixels'
+
+describe('Product Class', () => {
+  it('Creates an instance of RemoveBlackPixels', () => {
+    const matrix = [
+      [1, 0, 1, 0, 0],
+      [0, 1, 0, 1, 1],
+      [0, 1, 0, 1, 0],
+      [0, 1, 0, 1, 0],
+      [0, 0, 0, 0, 1]
+    ]
+    const removeBlackPixels: RemoveBlackPixels = new RemoveBlackPixels(matrix)
+
+    expect(removeBlackPixels).toBeDefined()
+    expect(removeBlackPixels.inputMatrix).toEqual(matrix)
+    expect(removeBlackPixels.regionData).toEqual({})
+    expect(removeBlackPixels.regionMatrix).toEqual([
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]
+    ])
+  })
+
+  it('Tags individual regions with natural numbers from 1 to N, where N is the total number of regions', () => {
+    const matrix = [
+      [1, 0, 1, 0, 0],
+      [0, 1, 0, 1, 0],
+      [0, 0, 0, 0, 0],
+      [0, 1, 0, 1, 0],
+      [0, 0, 1, 0, 1]
+    ]
+    const removeBlackPixels: RemoveBlackPixels = new RemoveBlackPixels(matrix)
+
+    const { regionsMatrix } = removeBlackPixels.getRegionMatrix()
+
+    expect(regionsMatrix).toEqual([
+      [1, 0, 2, 0, 0],
+      [0, 3, 0, 4, 0],
+      [0, 0, 0, 0, 0],
+      [0, 5, 0, 6, 0],
+      [0, 0, 7, 0, 8]
+    ])
+  })
+})
--- src/models/RemoveBlackPixels.ts
+++ src/models/RemoveBlackPixels.ts
@@ -1,42 +1,48 @@
 interface IRegionData {
-  regionName: number
-  regionSize: number
-  isConnectedToBorder: boolean
+  [key: string]: {
+    regionName: number
+    regionSize: number
+    isConnectedToBorder: boolean
+  }
 }
 export class RemoveBlackPixels {
-  taggedRegions: number[][]
-  regionData: Array<IRegionData>
+  regionMatrix: number[][]
+  inputMatrix: number[][]
+  regionData: IRegionData
 
-  constructor(n: number, m: number) {
-    this.taggedRegions = this.createEmptyMatrix(n, m)
-    this.regionData = []
+  constructor(matrix: number[][]) {
+    this.regionMatrix = this.createEmptyMatrix(matrix.length, matrix[0].length)
+    this.inputMatrix = matrix
+    this.regionData = {}
   }
 
-  public createEmptyMatrix(n: number, m: number): number[][] {
+  private createEmptyMatrix(n: number, m: number): number[][] {
     return new Array(n).fill(0).map(() => new Array(m).fill(0))
   }
 
   public doDFS(matrix: number[][], row: number, col: number, currentRegion: number): void {
-    if (this.taggedRegions[row][col]) {
+    if (this.regionMatrix[row][col]) {
       return
     }
+    console.log(row, col)
 
     // mark this vertex as visited and visit all its neighbors in depth first manner
-    this.taggedRegions[row][col] = currentRegion
+    this.regionMatrix[row][col] = currentRegion
   }
 
-  getTaggedRegions(matrix: number[][]): number[][] {
+  getRegionMatrix(): { regionsMatrix: number[][]; regionData: IRegionData } {
     let currentRegion = 1
 
-    for (let row = 0; row < matrix.length; row++) {
-      for (let col = 0; col < matrix[0].length; col++) {
-        if (matrix[row][col] === currentRegion && !this.taggedRegions[row][col]) {
-          this.doDFS(matrix, row, col, currentRegion)
+    // Travese through the given matrix to find all the vextexes
+    for (let row = 0; row < this.inputMatrix.length; row++) {
+      for (let col = 0; col < this.inputMatrix[0].length; col++) {
+        if (this.inputMatrix[row][col] === 1 && this.regionMatrix[row][col] !== currentRegion) {
+          this.doDFS(this.inputMatrix, row, col, currentRegion)
           currentRegion = currentRegion + 1
         }
       }
     }
 
-    return this.taggedRegions
+    return { regionsMatrix: this.regionMatrix, regionData: this.regionData }
   }
 }
